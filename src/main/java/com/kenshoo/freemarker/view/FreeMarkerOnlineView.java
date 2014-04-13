f7981ac685fb06ab6d65bdc47bfb7e657ca4098a
nirfeldman <nir.feldman@kenshoo.com>
2014-04-13 17:10:21 +0300

    Added UI

--- src/main/java/com/kenshoo/freemarker/view/FreeMarkerOnlineView.java
+++ src/main/java/com/kenshoo/freemarker/view/FreeMarkerOnlineView.java
@@ -10,7 +10,38 @@ import com.yammer.dropwizard.views.View;
  */
 public class FreeMarkerOnlineView extends View {
 
+    private final boolean errorInTemplateEvaluation;
+    private final String template;
+    private final String params;
+    private String result;
+    public static final String ERROR_IN_EVAL = "Error in template Evaluation: ";
+    public static final String NO_RESULTS = "No Results Yet ";
+
+
     public FreeMarkerOnlineView() {
+                this(false,NO_RESULTS,"","");
+        }
+
+    public FreeMarkerOnlineView(boolean errorInTemplateEvaluation,String result, String template, String params) {
         super("/view/freemarker-online.mustache");
+        this.errorInTemplateEvaluation = errorInTemplateEvaluation;
+        this.result = result;
+        this.template = template;
+        this.params = params;
+
     }
+
+    public String getResult(){
+        if (errorInTemplateEvaluation)
+            return  ERROR_IN_EVAL + result;
+        return result;
+    }
+
+    public String getTemplate(){
+        return template;
+    }
+
+    public String getParams(){
+            return params;
+        }
 }
---
+++ src/test/java/com/kenshoo/freemarker/resources/FreeMarkerOnlineResultResourceTest.java
@@ -0,0 +1,51 @@
+package com.kenshoo.freemarker.resources;
+
+import com.kenshoo.freemarker.services.FreeMarkerService;
+import com.kenshoo.freemarker.services.FreeMarkerServiceResponse;
+import com.kenshoo.freemarker.view.FreeMarkerOnlineView;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyMap;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nir
+ * Date: 4/12/14
+ * Time: 11:23 PM
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class FreeMarkerOnlineResultResourceTest {
+
+    @InjectMocks
+    FreeMarkerOnlineResultResource freeMarkerOnlineResultResource;
+
+    @Mock
+    FreeMarkerService freeMarkerService;
+
+    @Test
+    public void testGoodResult() {
+        when(freeMarkerService.calculateFreeMarkerTemplate(anyString(), anyMap())).thenReturn(new FreeMarkerServiceResponse.Builder().successfulResponse("Result"));
+        FreeMarkerOnlineView view = freeMarkerOnlineResultResource.formResult("template", "params");
+        assertEquals(view.getResult(), "Result");
+        assertEquals(view.getTemplate(), "template");
+        assertEquals(view.getParams(), "params");
+
+    }
+
+    @Test
+    public void testWrongTemplate() {
+        when(freeMarkerService.calculateFreeMarkerTemplate(anyString(), anyMap())).thenReturn(new FreeMarkerServiceResponse.Builder().errorResponse("Error"));
+        FreeMarkerOnlineView view = freeMarkerOnlineResultResource.formResult("template", "params");
+        assertEquals(view.getResult(), "Error");
+        assertEquals(view.getTemplate(), "template");
+        assertEquals(view.getParams(), "params");
+
+    }
+}
